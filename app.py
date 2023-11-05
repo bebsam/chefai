05a5c0b3161d9ad807a41bdbb577821f0ef9e6c5
AyeshaAzim <ashaik89@my.centennialcollege.ca>
2023-11-05 14:51:03 -0500

    Project added

--- app.py
+++ app.py
@@ -1,64 +1,122 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymongo
-from flask import redirect, url_for
-from bson import ObjectId  # This is typically how you import ObjectId from pymongo
-
+import os
+import requests
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+
+# Secret key for sessions
+app.secret_key = os.urandom(24)
 
 API_URL = "https://api-inference.huggingface.co/models/flax-community/t5-recipe-generation"
 API_KEY = "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"
 
 # MongoDB connection setup
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB connection URI
-db = client["recipe_db"]  # Replace "recipe_db" with your preferred database name
-recipes_collection = db["recipes"]  # Collection for storing recipes
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["recipe_db"]
+recipes_collection = db["recipes"]
+users_collection = db["users"]
 
 @app.route("/", methods=["GET", "POST"])
+
+def landing():
+    if 'user' in session:
+        return redirect(url_for('index'))
+    return render_template("landing.html")
+
+@app.route("/home", methods=["GET", "POST"])
 def index():
+    if 'user' not in session:
+        return redirect(url_for('landing'))
+
+    ingredients = None
+    recipe = None
+
     if request.method == "POST":
         ingredients = request.form.get("ingredients")
         if ingredients:
-            output = query_model(ingredients)
-            return render_template("index.html", ingredients=ingredients, recipe=output)
-    return render_template("index.html")
+            try:
+                recipe = query_model(ingredients)
+            except Exception as e:
+                recipe = f"An error occurred: {e}"
 
-# Create a route to display the recipe list
+    return render_template("index.html", ingredients=ingredients, recipe=recipe)
+
 @app.route("/recipe_list")
 def recipe_list():
-    # Retrieve recipes from MongoDB and pass them to the template
-    recipes = recipes_collection.find()
-    return render_template("recipe_list.html", recipes=recipes)
+    if 'user' not in session:
+        return redirect(url_for('login'))
 
-from flask import redirect, url_for
-from bson.objectid import ObjectId  # Correct import statement for ObjectId
+    recipes = recipes_collection.find({"username": session["user"]})
+    return render_template("recipe_list.html", recipes=recipes)
 
 @app.route("/delete_recipe/<recipe_id>", methods=["POST"])
 def delete_recipe(recipe_id):
-    # Logic to delete the recipe from the database using recipe_id
-    recipes_collection.delete_one({'_id': ObjectId(recipe_id)})
-    # Redirect to the recipe list page after deletion
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    try:
+        recipes_collection.delete_one({'_id': ObjectId(recipe_id), 'username': session['user']})
+    except Exception as e:
+        app.logger.error(f"Error deleting recipe: {e}")
+        return redirect(url_for('recipe_list'))
     return redirect(url_for('recipe_list'))
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+        hashed_password = generate_password_hash(password)
+        users_collection.insert_one({"username": username, "password": hashed_password})
+
+        return redirect(url_for('login'))
+    
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = users_collection.find_one({"username": username})
+        
+        if user and check_password_hash(user["password"], password):
+            session["user"] = user["username"]
+            return redirect(url_for('index'))
+        else:
+            return "Invalid username or password"
+    
+    return render_template("login.html")
+
+@app.route('/login_as_guest', methods=['GET', 'POST'])
+def login_as_guest():
+    session['user'] = 'guest' 
+    return redirect(url_for('index')) 
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('landing'))
 
 def query_model(ingredients):
     headers = {"Authorization": f"Bearer {API_KEY}"}
-    payload = {
-        "inputs": ingredients,
-    }
+    payload = {"inputs": ingredients}
     response = requests.post(API_URL, headers=headers, json=payload)
-
-    # Extract the generated text from the JSON response
     response_json = response.json()
     generated_text = response_json[0].get("generated_text", "")
 
-    # Store the generated recipe in MongoDB
-    recipe_data = {"ingredients": ingredients, "recipe": generated_text}
+    recipe_data = {
+        "ingredients": ingredients,
+        "recipe": generated_text,
+        "username": session["user"]
+    }
     recipes_collection.insert_one(recipe_data)
 
-    return generated_text  # Return the generated recipe text without "generated text"
+    return generated_text
 
 if __name__ == "__main__":
     app.run(debug=True)
